99306bba12e36c963f120b351295fb4e2f92c268
ericbrun <ericbrun47@gmail.com>
2019-07-30 10:34:12 +0200

    Script en cours
    Creation d'une VM avec Azure Powershell

---
+++ script.ps1
@@ -0,0 +1,38 @@
+#Generate a ssh key
+ssh-keygen -t rsa -b 2048 -f $HOME/.ssh/id_rsa 
+New-AzResourceGroup -Name "myResourceGroup" -Location "francecentral"
+
+
+# Define a credential object
+$securePassword = ConvertTo-SecureString ' ' -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ("azureuser", $securePassword)
+
+
+# Create a virtual machine configuration
+$vmConfig = New-AzVMConfig `
+  -VMName "myVM" `
+  -VMSize "Standard_D1" | `
+Set-AzVMOperatingSystem `
+  -Linux `
+  -ComputerName "myVM" `
+  -Credential $cred `
+  -DisablePasswordAuthentication | `
+Set-AzVMSourceImage `
+  -PublisherName "Canonical" `
+  -Offer "UbuntuServer" `
+  -Skus "16.04-LTS" `
+  -Version "latest"  `
+#Add-AzVMNetworkInterface `
+#  -Id $nic.Id
+
+# Configure the SSH key
+$sshPublicKey = cat ~/.ssh/id_rsa.pub
+Add-AzVMSshPublicKey `
+  -VM $vmconfig `
+  -KeyData $sshPublicKey `
+  -Path "/home/$env:UserName/.ssh/authorized_keys"
+
+
+New-AzVM `
+  -ResourceGroupName "myResourceGroup" `
+  -Location eastus -VM $vmConfig
