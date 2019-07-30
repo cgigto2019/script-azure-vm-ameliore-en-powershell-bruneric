e4d5d890f536344e6c19c74c5af0d5f32b6bbad5
ericbrun <ericbrun47@gmail.com>
2019-07-30 10:55:19 +0200

    Script update

--- script.ps1
+++ script.ps1
@@ -3,6 +3,70 @@ ssh-keygen -t rsa -b 2048 -f $HOME/.ssh/id_rsa
 New-AzResourceGroup -Name "myResourceGroup" -Location "francecentral"
 
 
+# Create a subnet configuration
+$subnetConfig = New-AzVirtualNetworkSubnetConfig `
+  -Name "mySubnet" `
+  -AddressPrefix 192.168.1.0/24
+
+# Create a virtual network
+$vnet = New-AzVirtualNetwork `
+  -ResourceGroupName "myResourceGroup" `
+  -Location "francecentral" `
+  -Name "myVNET" `
+  -AddressPrefix 192.168.0.0/16 `
+  -Subnet $subnetConfig
+
+# Create a public IP address and specify a DNS name
+$pip = New-AzPublicIpAddress `
+  -ResourceGroupName "myResourceGroup" `
+  -Location "francecentral" `
+  -AllocationMethod Static `
+  -IdleTimeoutInMinutes 4 `
+  -Name "mypublicdns$(Get-Random)"
+
+
+# Create an inbound network security group rule for port 22
+$nsgRuleSSH = New-AzNetworkSecurityRuleConfig `
+  -Name "myNetworkSecurityGroupRuleSSH"  `
+  -Protocol "Tcp" `
+  -Direction "Inbound" `
+  -Priority 1000 `
+  -SourceAddressPrefix * `
+  -SourcePortRange * `
+  -DestinationAddressPrefix * `
+  -DestinationPortRange 22 `
+  -Access "Allow"
+
+# Create an inbound network security group rule for port 80
+$nsgRuleWeb = New-AzNetworkSecurityRuleConfig `
+  -Name "myNetworkSecurityGroupRuleWWW"  `
+  -Protocol "Tcp" `
+  -Direction "Inbound" `
+  -Priority 1001 `
+  -SourceAddressPrefix * `
+  -SourcePortRange * `
+  -DestinationAddressPrefix * `
+  -DestinationPortRange 80 `
+  -Access "Allow"
+
+# Create a network security group
+$nsg = New-AzNetworkSecurityGroup `
+  -ResourceGroupName "myResourceGroup" `
+  -Location "francecentral" `
+  -Name "myNetworkSecurityGroup" `
+  -SecurityRules $nsgRuleSSH,$nsgRuleWeb
+  
+  
+# Create a virtual network card and associate with public IP address and NSG
+$nic = New-AzNetworkInterface `
+  -Name "myNic" `
+  -ResourceGroupName "myResourceGroup" `
+  -Location "francecentral" `
+  -SubnetId $vnet.Subnets[0].Id `
+  -PublicIpAddressId $pip.Id `
+  -NetworkSecurityGroupId $nsg.Id
+  
+
 # Define a credential object
 $securePassword = ConvertTo-SecureString ' ' -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ("azureuser", $securePassword)
@@ -35,4 +99,4 @@ Add-AzVMSshPublicKey `
 
 New-AzVM `
   -ResourceGroupName "myResourceGroup" `
-  -Location eastus -VM $vmConfig
+  -Location francecentral -VM $vmConfig
